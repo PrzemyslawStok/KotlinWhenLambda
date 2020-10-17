a9c4ab2ac8f161c2e2f3a6d854e29b351052a753
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-17 12:42:59 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,22 @@
 fun main() {
-    val x = 2323
+    val x = 17
 
     when(x){
-        0->println("x wynosi zero")
+        0->{println("x wynosi zero")}
         1,2,3,4,5,6->println("x należy do zbioru {1,2,3,4,5,6} ")
+        in 8..20->println("x zawiera się w przedziale 8..20")
         else->println("kolejna funkcja")
     }
+
+    val y = 5
+
+    val z = when(y){
+        0->2
+        in 20..40->2
+        2,3,4,5,6->5
+        else->1
+    }
+
+    println("z wynosi ${z}")
+
 }
