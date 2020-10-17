18bca361550f1d070ea2dd35e035d1a3cf095dbd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-17 12:30:59 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
 fun main() {
-    val x = 1
+    val x = 2323
 
     when(x){
         0->println("x wynosi zero")
         1,2,3,4,5,6->println("x należy do zbioru {1,2,3,4,5,6} ")
+        else->println("kolejna funkcja")
     }
 }
