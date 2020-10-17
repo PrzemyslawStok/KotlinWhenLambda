06c3d8130a54c144dc70e4b59b6900e78aa96553
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-17 12:28:01 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World!")
+    val x = 100
+
+    when(x){
+        0->println("x wynosi zero")
+        0,1,2,3,4,5,6->("x należy do zbioru {0,1,2,3,4,5,6} ")
+    }
 }
