dbf31e4f37bd882cb2edf81142ab78c76f4a4562
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-17 13:08:04 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,28 @@
+import java.lang.Math.exp
+
 fun main() {
     val x = 17
 
-    when(x){
-        0->{println("x wynosi zero")}
-        1,2,3,4,5,6->println("x należy do zbioru {1,2,3,4,5,6} ")
-        in 8..20->println("x zawiera się w przedziale 8..20")
-        else->println("kolejna funkcja")
+    val result = when(x){
+        0->{
+            println("x wynosi zero")
+            2
+        }
+        1,2,3,4,5,6->{
+            println("x należy do zbioru {1,2,3,4,5,6} ")
+            5
+        }
+        in 8..20-> {
+            println("x zawiera się w przedziale 8..20")
+            15
+        }
+        else-> {
+            println("kolejna funkcja")
+            8
+        }
     }
+
+    println("result ${result}")
 
     val y = 21
 
@@ -21,6 +37,18 @@ fun main() {
 
     z = if(x>y) 5 else 10
 
+    example1()
+}
+
+fun example1(){
+    //x-e^(-x) = 0
+    var x = 0.0
+    var result = x - exp(-x)
+    println("$x-e^(-$x)=${result}")
+    while(x<100){
+        x = x+1
+        //println(x)
+    }
 }
 
 
